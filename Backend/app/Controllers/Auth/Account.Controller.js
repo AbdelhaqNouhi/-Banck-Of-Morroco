100a192225c2d299c6d131408c24d4daad46bf48
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-01-04 15:22:30 +0100

    added account crud

---
+++ Backend/app/Controllers/Auth/Account.Controller.js
@@ -0,0 +1,50 @@
+const asyncHandler = require('express-async-handler');
+const { model } = require('mongoose');
+const AccountModule = require('../../Models/Account.Model');
+
+const GetAcountById = asyncHandler(async (req, res) => {
+    
+        try {
+            const account = await AccountModule.findById(req.params.id);
+            res.status(201).json(account)
+    
+        } catch (err) {
+            console.error('Error: ' + err.message)
+            res.status(401).json({ status: "fail" , message: err.message})
+        }
+})
+
+const UpdateAccount = asyncHandler(async (req, res) => {
+
+    const account = await AccountModule.findById(req.params.id);
+
+    if (!account) {
+        res.status(401).json({ status: "fail" , message: "Account not found" })
+    }
+
+    const UpdateAccount = await AccountModule.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(201).json(UpdateAccount)
+})
+
+const DeleteAccount = asyncHandler(async (req, res) => {
+    
+        const account = await AccountModule.findById(req.params.id);
+    
+        if (!account) {
+            res.status(401).json({ status: "fail" , message: "Account not found" })
+        }
+    
+        await account.remove()
+    
+        res.status(201).json({ id: req.params.id, message: "Account deleted"})
+})
+
+model.exports = {
+    GetAcountById,
+    UpdateAccount,
+    DeleteAccount
+}
