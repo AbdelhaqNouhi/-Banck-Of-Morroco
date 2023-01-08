0731d68c761766463560e3534c7f2316c9ac5cd8
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-01-09 00:53:01 +0100

    fin

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import Background from "../assets/images/slider.jpg";
 import Typewriter from "typewriter-effect";
+import PrimaryButton from "../layouts/Button/PrimaryButton";
+import { useState } from "react";
 
 const Home = () => {
+    const [title, setTitle] = useState('OPEN MY ACCOUNT');
+
 
     return (
         <div>
             <div className="absolute flex flex-col gap-12 justify-center my-28 mx-16 te text-4xl font-bold text-blue-500">
-                <div>
+                {/* <div> */}
                     <Typewriter
                         onInit={(typewriter) => {
                             typewriter
                                 .typeString(
-                                    "Welcom Back!! 🖐 <br/> <br/> Becoming a CRÉDIT DU MAROC, <br/> customer in all simplicity."
+                                    "Welcom Back!! 🖐"
                                 )
                                 .pauseFor(2500)
                                 .start();
                         }}
                     />
-                </div>
+                    <div className="relative flex flex-col gap-12">
+                        <h1>Becoming a CRÉDIT DU MAROC, <br/> customer in all simplicity.</h1>
+                        <PrimaryButton title={ title } />
+                    </div>
+                {/* </div> */}
             </div>
             <div className="">
                 <img src={Background} alt="img" />
