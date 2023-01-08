82fec659aec8213905429a64abece62f3512ff55
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-01-08 20:11:16 +0100

    added content to first part in home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Background from "../assets/images/slider.jpg";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
     return (
-        <div>Home</div>
-    )
-}
+        <div>
+            <div className="absolute flex justify-center my-28 mx-16 te text-4xl text-bold">
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Welcom Back!! 🖐 <br/> <br/> Becoming a CRÉDIT DU MAROC, <br/> customer in all simplicity.")
+                            .pauseFor(2500)
+                            .start();
+                    }}
+                />
+            </div>
+            <div className="">
+                <img src={Background} alt="img" />
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
